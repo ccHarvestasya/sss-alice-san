eabd129ca46f496fa8afb1928d68463fefad6c15
ccHarvestasya <harvestasya84@gmail.com>
2024-08-16 23:14:19 +0900

    公開鍵要求

--- src/back.ts
+++ src/back.ts
@@ -1,61 +1,24 @@
 'use strict'
-// import { requestSign, setTransactionByPayload } from "sss-module";
 
-// chrome.webRequest.onErrorOccurred.addListener(
-//   function (details) {
-//     console.log("onErrorOccurred");
-//     console.log(details.url);
-//   },
-//   { urls: ["<all_urls>"] },
-//   []
-// );
-
-// chrome.webRequest.onBeforeRequest.addListener(
-//   function (details) {
-//     console.log("onBeforeRequest");
-//     console.log(details.url);
-//   },
-//   { urls: ["<all_urls>"] },
-//   []
-// );
-
-// chrome.webNavigation.onBeforeNavigate.addListener((event) => {
-//   console.log("onBeforeNavigate");
-//   console.log(event);
-// });
+// alice://sign?type=request_pubkey&callback=68747470733A2F2F76656E7475732D77616C6C65742E6E65742F706565726E6F64652F64656C65676174652E68746D6C3F667269656E646C794E616D653D456E6A6F79253230616E64253230737072656164253230506F5325324221286E6F6465342926686F73743D3133332E31382E3232382E373726686F737444657461696C3D4B796F746F25324325323032362532432532304A6170616E2676657273696F6E3D76312E302E332E37266E6F64655F7075626C69634B65793D34353633444432304441363432443032424231344134303631314631334346463532343131363333314242373030354638463737463646393030313345374237264E45545F547970653D4D61696E4E6574
 
 chrome.webNavigation.onErrorOccurred.addListener(async (event) => {
   console.log('onErrorOccurred')
   if (/^alice:/.test(event.url)) {
     const param = event.url.match(/^alice:\/\/sign(.*)/)
-    console.log(event.url.match(/^alice:\/\/sign(.*)/))
-    console.log(event.url)
-    console.log(getParams(param![1]))
+    console.log('1' + event.url.match(/^alice:\/\/sign(.*)/))
+    console.log('2' + event.url)
     const params = getParams(param![1])
-    if (params.data !== undefined) {
-      console.log(params.data)
+    console.log(params)
+
+    if (params.type !== undefined) {
       await chrome.tabs.sendMessage(event.tabId, {
         action: 'GET_WINDOW',
-        data: params.data,
+        data: params,
       })
-      // setTransactionByPayload(params.data);
-      // requestSign().then((payload) => {
-      //   console.log(payload);
-      // });
     }
-    // setPopupForTab();
   }
 })
-
-// function setPopupForTab() {
-//   chrome.windows.create({
-//     url: "popup.html?foo=bar",
-//     type: "popup",
-//     width: 320,
-//     height: 480,
-//   });
-//   // chrome.action.openPopup();
-// }
 
 const getParams = (params: string): { [key: string]: string } => {
   const paramsArray = params.slice(1).split('&')
--- src/embed.ts
+++ src/embed.ts
@@ -1,31 +1,42 @@
-import { requestSign, setTransactionByPayload } from 'sss-module'
+import { getActiveNetworkType, getActivePublicKey, requestSign, setTransactionByPayload } from 'sss-module'
+import { Buffer } from 'buffer'
 
+window.Buffer = window.Buffer || Buffer
 window.addEventListener(
   'message',
   (ev) => {
-    if (ev.source != window) {
-      return
-    }
+    if (ev.source != window) return
+
     if (ev.data.type && ev.data.type === 'FROM_CONTENT') {
       switch (ev.data.action) {
         case 'GET_WINDOW':
-          //   chrome.runtime.sendMessage({
-          //     action: ev.data.action,
-          //     data: ev.data.data,
-          //   });
-          console.log(ev.data.action)
-          console.log(ev.data.data)
-
-          setTransactionByPayload(ev.data.data)
-          requestSign().then((payload) => {
-            console.log(payload.payload)
-          })
-
+          console.log(ev.data.data.type)
+          switch (ev.data.data.type) {
+            case 'request_sign_transaction':
+              // TODO: method: "announce"の対応が必要
+              setTransactionByPayload(ev.data.data.data)
+              requestSign().then((signedTx) => {
+                console.log(signedTx.payload)
+              })
+              break
+            case 'request_pubkey':
+              console.log(ev.data.data.callback)
+              const callback = Buffer.from(ev.data.data.callback, 'hex').toString('utf-8')
+              const networkType = getActiveNetworkType() == 104 ? 'MainNet' : 'TestNet'
+              const callbackUrl = callback + `&pubkey=${getActivePublicKey()}&network=${networkType}`
+              console.log(callbackUrl)
+              location.href = callbackUrl
+              break
+            case 'request_sign_utf8':
+            case 'request_sign_binary_hex':
+            case 'request_sign_batches':
+            default:
+              console.log(`'${ev.data.data.type}' は、未対応です。`)
+              break
+          }
           break
       }
     }
-    console.log('embed')
-    console.log(ev)
   },
   false
 )
